dde01ea7fb63ec63ec35ba840ded5ad17668bb98
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-21 00:12:25 -0500

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*using puts to print hello world*/
+char mj = "\"Programming is like building a multilingual puzzle";
+puts(mj);
+return (0);
+}
+
