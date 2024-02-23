dbd57e5dd1329c78a4676779c21cbfa35cc3f3a3
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-23 03:04:35 -0500

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Count 1 to 10
+  * Return: (value = 0)
+  */
+int main(void)
+{
+	/*variable int to save*/
+	int i;
+
+	/*Loop star from zero to 10*/
+	for (i = 0; i < 10; ++i)
+	{
+		printf("%d", i);
+	}
+	/*New line*/
+	printf("\n");
+
+	return (0);
+}
