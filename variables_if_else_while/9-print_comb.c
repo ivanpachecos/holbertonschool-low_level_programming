4233605d67d040df48e46617e2b025b10e5957e0
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-23 19:04:29 -0500

    combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Print only use putchar fuction and combinations
+  * Return: (value = 0)
+  */
+int main(void)
+{
+	int i;
+	/*loop for gives combinations of numbers and characteres*/
+	for (i = 0; i < 10; i++)
+	{
+		if (i > 0)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(i + '0');
+	}
+	/*finish with character $*/
+	putchar('$');
+	return (0);
+}
