8fb1b0de9242e0b05b99a2c19ec190fbcd3299a7
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-24 05:01:57 -0500

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,11 +2,12 @@
 
 /**
   * main - Prints the Alphabet
-  * Return: (value = 0)
+  * Return: Always 0.
   */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
