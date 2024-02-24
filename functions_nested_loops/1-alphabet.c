17bc505c2312a93ae2ae1efdb0e3ca6046bcf9de
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-24 04:51:51 -0500

    prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - Prints the Alphabet
+  * Return: (value = 0)
+  */
+void print_alphabet()
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
