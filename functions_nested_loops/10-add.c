ce053537c678fbd03a37eab18800a3e80d9c6db1
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-24 22:43:14 -0500

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+/**
+  * _abs - gives as absolute value of an integer with stdlib.h library
+  * @x: gives numbers of other fuction
+  * Return: (value integer numbers)
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
