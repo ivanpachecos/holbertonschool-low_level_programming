417239fae15b99cefeca7a34ef95597280cb1843
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-24 20:04:38 -0500

    value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+  * _abs - gives as absolute value of an integer with stdlib.h library
+  * @x: gives numbers of other fuction
+  * Return: (value integer numbers)
+  */
+int _abs(int x)
+{
+	return (abs(x));
+
+}
