47d04c56654775f8c721d46b17608fcb7f180337
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-27 18:05:34 -0500

    checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+
+/**
+  * _isdigit - checks if is digit or not
+  * @c: get data of other file
+  * Return: Return the code with iterator the codition
+  */
+int _isdigit(int c)
+{
+	return (isdigit(c) ? 1 : 0);
+
+}
