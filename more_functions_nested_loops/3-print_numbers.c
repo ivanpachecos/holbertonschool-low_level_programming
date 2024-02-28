6228601ab763a6d919eaaa0e48526391caabe9ae
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-28 11:07:25 -0500

    prints the numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-  * print_numbers: print 0 to 9
+  * print_numbers - prints 0 to 9
   */
 void print_numbers(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
 	{
 		_putchar(n + '0');
