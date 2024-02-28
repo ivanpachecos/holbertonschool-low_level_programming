d006a359c51521d8026b2c6d681dd1e87c03d3c1
ivanpachecos <ivanpachecos@yahoo.com>
2024-02-27 20:38:42 -0500

    multiplication with two value

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * mul - multiplication of two numbers
+  * @a: first numbers
+  * @b:	second numbes
+  * Return: result of the multiplication
+  */
+int mul(int a, int b)
+{
+	int result_multiplication;
+	result_multiplication = a * b;
+	return (result_multiplication);
+}
