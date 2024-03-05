87d24c579b69dd9097ec3494abdcbe9c17267123
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-05 14:25:46 -0500

    half string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,29 +5,21 @@
   *
   * @str: string
   */
-
-int _strlen(char *str)
-{
-    int length = 0;
-
-    while (*str != '\0')
-    {
-        length++;
-        str++;
-    }
-
-    return length;
-}
-
 void puts_half(char *str)
 {
-    int length = _strlen(str);
-    int i;
-    int start = (length - 1) / 2;
+	int long_str, half;
 
-    for (i = start; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	long_str = 0;
+	while (str[long_str] != '\0')
+		long_str++;
+
+	half = long_str / 2;
+
+	while (half < long_str)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+
 }
