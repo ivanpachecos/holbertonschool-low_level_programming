9ac694da42062021b81cc8e2d07d073c0953d287
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-05 18:51:55 -0500

    prints arr

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *
+  *
+  *
+  */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	
+	while (i < n)
+	{
+		i++;
+		printf("%d", a[i - 1]);
+		if (i < n - 1)
+			printf(", ");
+		
+	}
+	printf("\n");
+}
