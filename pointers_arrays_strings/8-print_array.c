1e6e1df2110c1f5308c6f84e9103f5bf14443780
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-05 18:58:30 -0500

    prints arr

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
-  *
-  *
-  *
+  * print_array - print number with coma
+  * @n: arr
+  * @a: arr[n]
   */
 
 void print_array(int *a, int n)
 {
 	int i = 0;
-	
+
 	while (i < n)
 	{
 		i++;
 		printf("%d", a[i - 1]);
 		if (i < n)
 			printf(", ");
-		
 	}
+
 	printf("\n");
 }
