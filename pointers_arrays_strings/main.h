031472194f985ff7119de8d0b4855ee25a73c2a3
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-07 19:20:03 -0500

    update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,6 +12,17 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+/*C - More pointers, arrays and strings*/
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+
 /*C - Even more pointers, arrays and strings*/
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
