bf4984f558ae33db388e4f8741a6bb4755aa2e6f
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-08 14:49:30 -0500

    searches a string for any of a set of bytes

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
+  * _strpbrk - searches a string for any of a set of bytes.
   *
+  * @s: locates the first occurrence in the string.
+  * @accept: compare bytes
   *
-  *
+  * Return: a pointer to the byte and NULL if no such byte is found.
   */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			/*Return the postion of the match in s*/
 			if (s[i] == accept[j])
-				return &s[i];
+				return (&s[i]);
 		}
 	}
-	return NULL;
+	return (NULL);
 }
