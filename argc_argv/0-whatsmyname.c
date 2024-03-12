d83eb49ff06a1d23cea72e08c3a37a032c9858b0
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-12 13:35:17 -0500

    prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+  * main - print its name, followed by a new line.
+  *
+  * @argc: is greater than zero.
+  * @argv: will be string containing the program's name.
+  *
+  * Return: always zero.
+  */
+
+int main(int argc, char *argv[])
 {
-	printf("\%s", argv[0]);
-	return 0;
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
 }
