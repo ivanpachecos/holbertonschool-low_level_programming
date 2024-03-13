ac5c7cc34a1a418a63146554383f88b5b55f61ee
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-13 13:39:31 -0400

    readme and main.h file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,10 +12,15 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 
 	str = malloc(sizeof(char) * (size + 1));
+	
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
