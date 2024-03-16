ab84b86d48c9d39e76f75424a58a7994547897a7
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-16 03:37:39 -0400

    allocates memory for an array, using malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
+
+/**
+  * _calloc -  allocates memory for an array, using malloc.
+  *
+  * @nmemb: specifies the number of elements you wish to assign.
+  * @size: size of each element in bytes
+  *
+  * Return: a pointer to the allocated memory.
+  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-   	unsigned int total_size, i; 
+	unsigned int total_size, i;
 	char *p;
+
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
 	total_size = nmemb * size;
 
 	p = (char *)malloc(total_size);
-    
-	if (p == NULL)
-		return NULL; 
-   
-	for (i = 0; i < total_size; i++) {
-		p[i] = 0;
-	}
 
- 
-	return p;
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < total_size; i++)
+		p[i] = 0;
+
+	return (p);
 }
