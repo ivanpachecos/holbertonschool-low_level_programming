bc1ab01ec3fcbeb404ced4cee6b11f816719f001
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-15 22:17:07 -0400

    allocates memory using malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *
+  *
+  *
+  *
+  *
+  *
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);	
+}
