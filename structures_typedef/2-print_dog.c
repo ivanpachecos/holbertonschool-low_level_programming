b7414c71949b16f0f0e076281fefeec06f94393d
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-16 23:40:46 -0400

    fucntion the print data of a dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * print_dog - print the characteristics of a dog
+  *
+  * @d: structure pointer
+  */
+
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+		return;
+
+	printf("Name: ");
+	if (d->name == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d->name);
+
+	printf("Age: ");
+	if (d->age == -1)
+		printf("(nil)\n");
+	else
+		printf("%.1f\n", d->age);
+
+	printf("Owner: ");
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d->owner);
+}
