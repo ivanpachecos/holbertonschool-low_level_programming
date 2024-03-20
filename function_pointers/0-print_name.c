0902d7226a3750605b658fce7265bccb43fe2197
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-20 01:08:08 -0400

    prints name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
 
 /**
+  * print_name - print name normal and uppercase.
   *
-  *
-  *
-  *
-  *
+  * @name: string of a name.
+  * @f: name assigned to the functions.
   */
 
 void print_name(char *name, void (*f)(char *))
