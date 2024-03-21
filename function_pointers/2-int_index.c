6a51fbf9ca305e489f22e4f7451a3c6bc9ab97a6
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-21 18:17:31 -0400

    index of an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+  * int_index - position of an number compared.
+  *
+  * @array: array of the elements.
+  * @size: of an array.
+  * @cmp: pointer function to compare
+  *
+  * Return: position of value.
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
