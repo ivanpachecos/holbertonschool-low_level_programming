df374ff8d76afcddbc65db13dc150c31f80251a4
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-22 23:47:26 -0400

    operation

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,33 +1,57 @@
 #include "3-calc.h"
 
 /**
-  *
-  *
-  *
-  *
-  *
+  * op_add - funtion add
+  * @a: number one
+  * @b: number two
+  * Return: add between two numbers
   */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+  * op_sub - sustrat function
+  * @a: number one
+  * @b: number two
+  * Return: sustrab between two numbers
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+  * op_mul - fucntion of multiplication
+  * @a: number one
+  * @b: number two
+  * Return: multiplication between two numbers
+  */
 int op_mul(int a, int b)
 {
-	return (a * b);	
+	return (a * b);
 }
 
+/**
+  * op_div - function div
+  *
+  * @a: number one
+  * @b: number two
+  * Return: div bettweem two numbers
+  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+/**
+  * op_mod - function mod
+  *
+  * @a: number one
+  * @b: number two
+  * Return: mod between two numbers
+  */
 int op_mod(int a, int b)
 {
 	return (a % b);
