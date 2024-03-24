94559c03f88ac7963236297359f889ddfc431af9
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-23 20:07:01 -0400

    operation

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef _VARIADIC_FUNCTION_H_
 #define _VARIADIC_FUNCTION_H_
-
+#include <stdarg.h>
 void print_numbers(const char *separator, const unsigned int n, ...);
 
+
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
 #endif
