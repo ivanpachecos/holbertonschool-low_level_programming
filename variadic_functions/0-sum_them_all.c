f5681095a89d9479ebde274ae2df6456de670877
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-23 20:12:47 -0400

    variadic function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,15 +1,13 @@
-#include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
-  *
-  *
-  *
-  *
-  *
-  *
-  */
-
-void print_numbers(const char *separator, const unsigned int n, ...)
+ * sum_them_all - sums all arguments passed
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum;
@@ -20,5 +18,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		sum += va_arg(list, int);
 
 	va_end(list);
-	return (sum);	
+	return (sum);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,12 +1,18 @@
-#ifndef _VARIADIC_FUNCTION_H_
-#define _VARIADIC_FUNCTION_H_
+#ifndef VARIADICFUNCTIONSH
+#define VARIADICFUNCTIONSH
 #include <stdarg.h>
-void print_numbers(const char *separator, const unsigned int n, ...);
 
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+int _putchar(char c);
 
 typedef struct funckey
 {
 	void (*f)(va_list);
 	char spec;
 } funckey;
+
 #endif
