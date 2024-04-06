d5ed6ebf7ea07640cf3f0dc2303fec1477e6702d
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-06 14:02:05 -0500

    frees memorty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,8 +6,9 @@ void free_list(list_t *head)
 	list_t *frees;
 	while (head != NULL)
 	{
-		frees = head;
-		head = head->next;
-		free(frees);
+		frees = head->next;
+		free(head->next);
+		free(head);
+		head = frees;
 	}
 }
