595e5fd677e002ad1bc0bdbe945d8a5f656ac932
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-06 15:50:14 -0400

    modified the function free list_t

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_list - free memory
+ *
+ * @head: data of the elemets
+ *
+ * Return: no return
+ */
+
 void free_list(list_t *head)
 {
 	list_t *freed;
