784bbffe3eefe87ad6889153cf192c2d5c96a740
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-07 17:08:49 -0500

    modified with style betty

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
+  * get_dnodeint_at_index - get data with index of the elemets
   *
-  *
-  *
-  *
-  *
+  * @head: pointer
+  * @index: value integer
+  * Return: the number
   */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *new_node = NULL;
 	unsigned int i = 0;
-	
+
 	new_node = head;
 	while (head == NULL)
 		return (NULL);
