9a7acb7de433bcd33ffaec46fa0955679e8bfc0f
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-07 18:02:04 -0500

    sum of all elemets

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,25 +2,23 @@
 #include <stdio.h>
 
 /**
- * 
- * 
- * 
- * 
- * 
- * 
+ * sum_dlistint - sum all elements
+ *
+ * @head: data
+ *
+ * Return: sum of all the elements
 */
 int sum_dlistint(dlistint_t *head)
 {
-  int sum_total = 0;
+	int sum_total = 0;
 
-  if (head == NULL)
-    return (0);
-  
-  while (head != NULL)
-  {
-    sum_total += head->n;
-    head = head->next;
-  }
-  
-  return (sum_total);
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum_total += head->n;
+		head = head->next;
+	}
+	return (sum_total);
 }
