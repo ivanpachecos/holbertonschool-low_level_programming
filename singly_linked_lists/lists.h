53dd2b43f3153280894bf25b6fdc9063654833e7
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-07 00:26:16 -0400

    delete somthing prototype

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -21,11 +21,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
