b1a214d78e7ce982f85f1813a0497ebad585da54
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-10 11:30:10 -0500

    modified operator

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,11 +23,11 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len] != '\0')
 		len++;
 
-	fd = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
-	
+
 	write(fd, text_content, len);
 	
 	return (1);
