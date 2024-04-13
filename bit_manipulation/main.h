f0231d7188dab13afa79b8ba4626317feccdd00b
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-13 00:58:42 -0500

    add prototype of function

---
+++ bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef _BIT_MANIPULATION_
+#define _BIT_MANIPULATION_
+
+unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
+void prbinary(unsigned long int n);
+#endif
