a243a86c53e942d3e8c79a6c67ada5b19c4177d9
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-14 23:47:41 -0500

    add librabery

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,10 @@
 #ifndef _IO_HEAD_
 #define _IO_HEAD_
 
-#include <stddef.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
