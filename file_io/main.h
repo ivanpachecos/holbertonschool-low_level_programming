301c98daedfe01285da9e849544e8388c502f601
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-14 23:53:11 -0500

    add prototype

--- file_io/main.h
+++ file_io/main.h
@@ -10,4 +10,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(const char *s);
+char *create_buffer(char *file);
+void close_file(int file_descriptor);
+int main(int argc, char *argv[]);
+
 #endif
