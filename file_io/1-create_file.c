226e9b6a9109e08bbb82a7bc68bc1f29f90b33bf
ivanpachecos <ivanpachecos@yahoo.com>
2024-05-03 00:01:58 -0500

    file modified 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,30 +12,24 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, len = 0;
+	long int wrote;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content != NULL)
+	{
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+		while (text_content[len])
+			len++;
+		wrote = write(fd, text_content, len);
+		if (wrote == -1)
+			return (-1);
+	}
+	if (close(fd) == -1)
 		return (-1);
-
-	close(fd);
-
 	return (1);
 }
